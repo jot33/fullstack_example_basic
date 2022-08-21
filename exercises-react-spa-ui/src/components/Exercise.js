5a6aef17152b97a657616e8df79a82a890b68455
jot33 <jot33@users.noreply.github.com>
2022-08-20 20:32:40 -0700

    Exercise.js REACT component had bug in keys if user input duplicate data

--- exercises-react-spa-ui/src/components/Exercise.js
+++ exercises-react-spa-ui/src/components/Exercise.js
@@ -4,11 +4,11 @@ import { MdDeleteForever, MdEdit } from 'react-icons/md';
 function Exercise({ exercise, onDelete, onEdit }) {
   return(
     <tr>
-      <td key={exercise.name}>{exercise.name}</td>
-      <td key={exercise.reps}>{exercise.reps}</td>
-      <td key={exercise.weight}>{exercise.weight}</td>
-      <td key={exercise.unit}>{exercise.unit}</td>
-      <td key={exercise.date}>{exercise.date.substring(0,10)}</td>
+      <td key={exercise._id + exercise.name}>{exercise.name}</td>
+      <td key={exercise._id + exercise.reps}>{exercise.reps}</td>
+      <td key={exercise._id + exercise.weight}>{exercise.weight}</td>
+      <td key={exercise._id + exercise.unit}>{exercise.unit}</td>
+      <td key={exercise._id + exercise.date}>{exercise.date.substring(0,10)}</td>
       <td key={exercise._id + "edit"} className="clickable"><MdEdit onClick={() => onEdit(exercise)}/></td>
       <td key={exercise._id + "delete"} className="clickable"><MdDeleteForever onClick={ () => onDelete(exercise._id)}/></td>
     </tr>
